55fd940392597100bd4b6242bd43303c2cade5e1
Merakrun <merakrun@gmail.com>
2022-05-21 20:35:28 +0200

    Add helper files

--- cosmosvalmon.py
+++ cosmosvalmon.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
+import datetime
 import gc
 import json
+import urllib.request
+
 import pyjq
-import datetime
+from dictor import dictor
+
+# dictor  makes it easier to avoid errors when the requested json element is not present
+
 # This version uses Pushover for notifications.
 # Consider replacing it by https://github.com/dschep/ntfy
 # To get alerts via other channels
-from pushover import init, Client
-# requires `pip3 install python-pushover`
-# urllib.request module uses HTTP/1.1 and includes Connection:close header in its HTTP requests
-import urllib.request
-# this makes it easier to avoid errors when the requested json element is not present
-from dictor import dictor
+# from pushover import init, Client
+
 
 # You will likely need to install these modules
 # pip3 install websocket-client
@@ -25,14 +27,14 @@ from dictor import dictor
 yourval = "<your-validator-id>"
 # To test this enter below a validator that has been missing blocks frequently
 # and uncomment the line
-#yourval = "<flaky-validator-id>"
+# yourval = "<flaky-validator-id>"
 # When a lot of validators miss a block, it is not your fault. if you alone or only a handful missed,
 # you should look into your setup. Set alertIfLessThan to the max number of validators missing a block you'd tolerate
 # before you'd want to be alerted. If you set it to 0, you will never get an alert.
 alertIfLessThan = 5
 baseurl = "http://localhost:26657/block?height="
-logfile = "/path/to/log/cosmosvalmon.log"
-client = Client("<your-pushover-client-key>", api_token="<your-pushover-api-token>")
+logfile = "cosmosvalmon.log"
+# client = Client("<your-pushover-client-key>", api_token="<your-pushover-api-token>")
 
 # Output to stdout is
 # "block height, proposer ID, number of validators that missed the block"
@@ -41,26 +43,37 @@ client = Client("<your-pushover-client-key>", api_token="<your-pushover-api-toke
 # it will further append "X validators missed, below your threshold of Y"
 
 # No need to change anything from here on
-headers = {'content-type': 'application/json'}
+headers = {"content-type": "application/json"}
+
 
 def fetch_json(url):
     req = urllib.request.Request(url)
-    req.add_header = (headers)
+    req.add_header = headers
     response = urllib.request.urlopen(req)
     data = response.read()
     datajson = json.loads(data)
     return datajson
 
+
 from websocket import create_connection
+
 ws = create_connection("ws://localhost:26657/websocket")
-ws.send(json.dumps({"jsonrpc": "2.0", "method": "subscribe",
-                    "id": "1", "params": {"query": "tm.event='NewBlock'"}}))
+ws.send(
+    json.dumps(
+        {
+            "jsonrpc": "2.0",
+            "method": "subscribe",
+            "id": "1",
+            "params": {"query": "tm.event='NewBlock'"},
+        }
+    )
+)
 
 try:
     while True:
         blockdata = ws.recv()
         blockjson = json.loads(blockdata)
-        block = str(dictor(blockjson, 'result.data.value.block.header.height'))
+        block = str(dictor(blockjson, "result.data.value.block.header.height"))
         if block != "None":
 
             block = int(block)
@@ -75,14 +88,14 @@ try:
             url = baseurl + str(previousblock)
             result = fetch_json(url)
             # jq -r '.result.block.header.proposer_address'
-            proposer_address = (
-                dictor(result, 'result.block.header.proposer_address'))
+            proposer_address = dictor(result, "result.block.header.proposer_address")
 
             url = baseurl + str(block)
             # jq -r '.result.block.last_commit.precommits[].validator_address'
             result = fetch_json(url)
-            validator_addresses = (
-                pyjq.all('.result.block.last_commit.precommits[].validator_address', result))
+            validator_addresses = pyjq.all(
+                ".result.block.last_commit.precommits[].validator_address", result
+            )
 
             # Start out with the assumption that your val did miss the block
             missed = 1
@@ -105,19 +118,42 @@ try:
                 yourvalmissed = ", (including your validator)."
 
                 if valmissedcount < alertIfLessThan:
-                        alert = " Also sending an alert: " + str(valmissedcount) + " validators missed - below your threshold of " + str(alertIfLessThan)
-
-                log = (now.strftime("%Y-%m-%d-%H:%M:%S") + ', ' + str(previousblock) + ', ' + proposer_address + ', ' + str(valmissedcount) + yourvalmissed)
+                    alert = (
+                        " Also sending an alert: "
+                        + str(valmissedcount)
+                        + " validators missed - below your threshold of "
+                        + str(alertIfLessThan)
+                    )
+                log = (
+                    now.strftime("%Y-%m-%d-%H:%M:%S")
+                    + ", "
+                    + str(previousblock)
+                    + ", "
+                    + proposer_address
+                    + ", "
+                    + str(valmissedcount)
+                    + yourvalmissed
+                )
                 # write to log file
-                with open(logfile, 'a+') as f:
-                    f.write(log + alert + '\n')
+                with open(logfile, "a+") as f:
+                    f.write(log + alert + "\n")
 
-            print (str(previousblock) + ', ' + proposer_address + ', ' + str(valmissedcount) + yourvalmissed + alert, flush=True))
+            print(
+                str(previousblock)
+                + ", "
+                + proposer_address
+                + ", "
+                + str(valmissedcount)
+                + yourvalmissed
+                + alert,
+                flush=True,
+            )
 
             if alert != "":
-                client.send_message(log + alert, title="Cosmos Validator Missed Block")
-            
+                # client.send_message(log + alert, title="Cosmos Validator Missed Block")
+                print(log + alert)
+
             gc.collect()
 
 except KeyboardInterrupt:
-    print('............Exiting!')
+    print("............Exiting!")
